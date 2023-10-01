770f353c08afd7438c3ae85b78c10e19ea535e45
DavisLima81 <davislima81@gmail.com>
2023-10-01 08:47:46 -0300

    ajustes em permutas

--- resources/views/filament/resources/escalados/pages/view-escalado.blade.php
+++ resources/views/filament/resources/escalados/pages/view-escalado.blade.php
@@ -3,32 +3,93 @@
     <div class="card w-full bg-gray-800 text-white">
         <div class="card-body items-center text-center">
             <h2 class="card-title">DETALHES DO ESCALADO</h2>
-            <p>{{ $this->record->escala->guarnicao->sigla . ' - ' . $this->record->escala->nome  . ' - ' .  $this->record->getEscaladoDia() }}</p>
+            <br><br>
+            <p>{{ 'SIGLA:  ' . $this->record->escala->guarnicao->sigla . ' - ' . $this->record->escala->nome}}</p>
+            <p>{{ 'DATA:  ' . $this->record->getEscaladoDia() }}</p>
         </div>
     </div>
-    <br><br>
+    <br>
+    <p>{{ 'DESCRIÇÃO:  ' . $this->record->escala->descricao  . ' - ' . $this->record->escala->guarnicao->descricao}}</p>
+    <br>
 
-<p class="text-white">
-    <strong class="text-primary-400">ORIGINAL:</strong> {{ strval($this->record->getEfetivoTrig()) }}
-    <br>
-    <strong class="text-primary-400">PERMUTA:</strong> @if($this->record->temPermuta()) SIM @else NÃO @endif
-    <br>
+    <div class="hero bg-base-400">
+        <div class="hero-content flex-col lg:flex-row justify-items-start items-start">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th class="text-center">DADOS</th>
+                    <th class="text-center"></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td class="text-primary-600">ORIGINAL:</td>
+                    <td class="text-center">{{ $this->record->getEfetivoTrig() }}</td>
+                </tr>
+                <tr>
+                    <td class="text-primary-600">PERMUTA:</td>
+                    <td class="text-center">@if($this->record->temPermuta()) SIM @else NÃO @endif</td>
+                </tr>
+                <tr>
+                    <td class="text-primary-600">PERMUTADO:</td>
+                    @if($this->record->temPermuta())
+                        <td class="text-center">{{ $this->record->efetivo_trig() }}</td>
+                    @else
+                        <td class="text-center">---</td>
+                    @endif
+                </tr>
+                </tbody>
+            </table>
+            <div class="divider divider-horizontal text-gray-600"></div>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th class="text-center">LEGENDA</th>
+                    <th class="text-center"></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td class="text-primary-600">ORIGINAL:</td>
+                    <td class="text-center">Trigrama do primeiro militar escalado</td>
+                </tr>
+                <tr>
+                    <td class="text-primary-600">PERMUTA:</td>
+                    <td class="text-center">Sim ou não. Registra se houve permuta para este escalado</td>
+                </tr>
+                <tr>
+                    <td class="text-primary-600">PERMUTADO:</td>
+                    <td class="text-center">Trigrama do militar escalado através da permuta mais recente</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="divider text-gray-600">#######</div>
+
     @if($this->record->temPermuta())
-        <strong class="text-primary-400">PERMUTADO:</strong> {{ strval($this->record->efetivo_trig()) }}
-        <br>
-        <br>
-    <hr>
-    <h1 class="py-3"><strong class="text-primary-400">PERMUTAS</strong></h1>
-    <p class="text-white py-2">
-        @foreach($this->record->getPermutas() as $permuta)
-            <strong class="text-primary-400">REGISTRADA:</strong> {{ $permuta->created_at->format('d/m/Y') }}
-            <br>
-            <strong class="text-primary-400">SAI:</strong> {{$permuta->saiEfetivoTrigrama() }}
-            <br>
-            <strong class="text-primary-400">ENTRA:</strong> {{ $permuta->entraEfetivoTrigrama() }}
-            <br>
-        @endforeach
-    </p>
-    @endif
 
+        <table class="table">
+            <thead>
+            <tr>
+                <th class="text-center">PERMUTAS</th>
+                <th class="text-center"></th>
+                <th class="text-center"></th>
+            </tr>
+            </thead>
+            <tbody>
+
+            @foreach($this->record->getPermutas() as $permuta)
+                <tr>
+                    <td class="text-primary-600">SAI: </td>
+                    <td>{{$permuta->saiEfetivoTrigrama() }}</td>
+                    <td class="text-primary-600">ENTRA: </td>
+                    <td>{{ $permuta->entraEfetivoTrigrama() }}</td>
+                    <td class="text-primary-600">REGISTRADA:</td>
+                    <td>{{ $permuta->created_at->format('d/m/Y') }}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    @endif
 </div>
