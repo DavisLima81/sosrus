9899922fac783f306c31b1a9e7705680d880ed8f
DavisLima81 <davislima81@gmail.com>
2023-10-19 06:33:20 -0300

    implementou widget EscaladoOverview

--- app/Filament/Resources/EscaladoResource/Widgets/EscaladoOverview.php
+++ app/Filament/Resources/EscaladoResource/Widgets/EscaladoOverview.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Resources\EscaladoResource\Widgets;
 
+use App\Models\Efetivo;
 use App\Models\Escala;
 use App\Models\Escalado;
+use App\Models\Guarnicao;
 use App\Models\Permuta;
+use App\Models\User;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Card;
 use Filament\Widgets\StatsOverviewWidget\Stat;
@@ -21,8 +24,23 @@ class EscaladoOverview extends BaseWidget
             COUNT(*) as total')->first();
         $permutaCount = Permuta::selectRaw('
             COUNT(*) as total')->first();
+        $efetivoCount = Efetivo::selectRaw('
+            COUNT(*) as total')->first();
+        $guarnicaoCount = Guarnicao::selectRaw('
+            COUNT(*) as total')->first();
+        $userCount = User::selectRaw('
+            COUNT(*) as total')->first();
 
         return [
+            Card::make('Usuários do sistema', $userCount->total)
+                ->color('citrus')
+                ->description('Total'),
+            Card::make('Efetivo da OBM', $efetivoCount->total)
+                ->color('citrus')
+                ->description('Total'),
+            Card::make('Guarnições (equipes)', $guarnicaoCount->total)
+                ->color('citrus')
+                ->description('Total'),
             Card::make('Serviços registrados', $escaladoCount->total)
                 ->color('primary')
                 ->description('Total'),
